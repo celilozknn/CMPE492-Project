09dd057e5b5d093025146cd04153364afb638165
celilozknn <celilozkan76@gmail.com>
2026-03-08 01:12:24 +0300

    chore: add more logs for infura fetcher

--- src/fetch_infura.py
+++ src/fetch_infura.py
@@ -1,10 +1,11 @@
-
-
 import requests
 import pandas as pd
 import time, os
 import dotenv
 import json
+
+from collections import Counter
+from datetime import datetime, timezone
 
 from helpers import *
 from paths import *
@@ -21,31 +22,15 @@ https://arbitrum-mainnet.infura.io/v3/{INFURA_API_KEY}
 https://avalanche-mainnet.infura.io/v3/{INFURA_API_KEY}
 """
 
-INFURA_URL = get_infura_url()
+def address_to_token_symbol_and_decimals(token_map: dict, address: str) -> tuple:
+    info = token_map.get(address.lower(), {"symbol": "Unknown", "decimals": 18})
+    return info["symbol"], info["decimals"]
 
-USDT_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.USDT).lower()
-USDC_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.USDC).lower()
-DAI_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.DAI).lower()
-XAUT_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.XAUT).lower()
-COIN_ADDRESSES = [USDT_ADDRESS, USDC_ADDRESS, DAI_ADDRESS, XAUT_ADDRESS]
-
-TRANSFER_EVENT_TOPIC = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"  # keccak256("Transfer(address,address,uint256)")
-
-
-def address_to_token_symbol(address: str) -> str:
-    TOKEN_SYMBOL_BY_ADDRESS = {
-        USDT_ADDRESS: "USDT",
-        USDC_ADDRESS: "USDC",
-        DAI_ADDRESS: "DAI",
-        XAUT_ADDRESS: "XAUT",
-    }
-    
-    return TOKEN_SYMBOL_BY_ADDRESS.get(address.lower(), "Unknown")
-    
-def fetch_logs(from_block=None, to_block=None, address=None, topics=None, logger=None):
+def fetch_logs(url, from_block=None, to_block=None, address=None, topics=None, logger=None):
     """
     Fetch logs from Infura using eth_getLogs.
     Parameters:
+        url (str): Infura endpoint URL.
         from_block (str): Hex string or tag (e.g., 'latest').
         to_block (str): Hex string or tag.
         address (str or list): Contract address or list of addresses.
@@ -64,7 +49,13 @@ def fetch_logs(from_block=None, to_block=None, address=None, topics=None, logger
     if topics:
         params['topics'] = topics
 
-    logger.info(f"Fetching logs with params: {params.items()}")
+    logger.info(
+        f"Fetching logs | fromBlock={hex_to_int(params.get('fromBlock')):,} "
+        f"toBlock={hex_to_int(params.get('toBlock')):,} "
+        f"number of addresses={len(params.get('address', []))} "
+        f"topics={params.get('topics')}"
+    )
+    
     payload_logs = {
         "jsonrpc": "2.0",
         "method": "eth_getLogs",
@@ -76,7 +67,7 @@ def fetch_logs(from_block=None, to_block=None, address=None, topics=None, logger
     headers = get_infura_headers()
 
     response = requests.post(
-        INFURA_URL,
+        url,
         data=json.dumps(payload_logs),
         headers=headers,
         auth=auth
@@ -92,10 +83,16 @@ def fetch_logs(from_block=None, to_block=None, address=None, topics=None, logger
     if 'error' in resp_json:
         logger.error(f"Error: {resp_json['error']}")
         return resp_json['error']
-    return resp_json.get('result', [])
+    
+    logs = resp_json.get('result', [])
+    logger.info(f"Received {len(logs)} logs from RPC")
+    if len(logs) > 9000:
+        logger.warning("Log count close to 10k limit. Consider reducing block chunk size.")
+    return logs
 
-def decode_log(log: dict) -> dict:
+def decode_log(token_map: dict, log: dict) -> dict:
     address = log["address"].lower()
+    token_symbol, token_decimals = address_to_token_symbol_and_decimals(token_map, address)
     
     return {
         "log_index": int(log["logIndex"], 16),
@@ -107,35 +104,81 @@ def decode_log(log: dict) -> dict:
         "block_timestamp": int(log["blockTimestamp"], 16),
 
         "contract_address": address,
-        "token_symbol": address_to_token_symbol(address),
+        "token_symbol": token_symbol,
 
         "topic": log["topics"][0],
         "from": ("0x" + log["topics"][1][-40:]).lower(),
         "to": ("0x" + log["topics"][2][-40:]).lower(),
         
-        "raw_value":  log["data"],
-        "value": int(log["data"], 16),
+        "raw_value": int(log["data"], 16),
+        "value": int(log["data"], 16) / (10 ** token_decimals),
 
         "raw_log": log
     }
 
 def main():
+        
+    ### CONFIGURE THE BLOCK RANGE ###
+    start_block = int_to_hex(24608077)
+    end_block = int_to_hex(24608077)
+    #################################
+    
     logger = get_logger("InfuraEventFetcher")
+
+    INFURA_URL = get_infura_url()
+
+    USDT_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.USDT).lower()
+    USDC_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.USDC).lower()
+    DAI_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.DAI).lower()
+    XAUT_ADDRESS = get_token_address(network=Networks.ETHEREUM, token=StableCoins.XAUT).lower()
+
+    USDT_DECIMALS = get_decimals(network=Networks.ETHEREUM, token=StableCoins.USDT)
+    USDC_DECIMALS = get_decimals(network=Networks.ETHEREUM, token=StableCoins.USDC)
+    DAI_DECIMALS = get_decimals(network=Networks.ETHEREUM, token=StableCoins.DAI)
+    XAUT_DECIMALS = get_decimals(network=Networks.ETHEREUM, token=StableCoins.XAUT)
+
+    COIN_ADDRESSES = [USDT_ADDRESS, USDC_ADDRESS, DAI_ADDRESS, XAUT_ADDRESS]
+        
+    TOKEN_MAPPING = {
+        USDT_ADDRESS: {"symbol": "USDT", "decimals": USDT_DECIMALS},
+        USDC_ADDRESS: {"symbol": "USDC", "decimals": USDC_DECIMALS},
+        DAI_ADDRESS: {"symbol": "DAI", "decimals": DAI_DECIMALS},
+        XAUT_ADDRESS: {"symbol": "XAUT", "decimals": XAUT_DECIMALS},
+    }
+
+    TRANSFER_EVENT_TOPIC = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"  # keccak256("Transfer(address,address,uint256)")
 
     validate_infura_api_credentials(logger)
     
     logger.info(f"Using Infura URL: {INFURA_URL}")
-
+    logger.info(f"Tracking contract addresses: {COIN_ADDRESSES}")
+    logger.info(f"Tracking tokens: {list(TOKEN_MAPPING.values())}")
+    logger.info(f"Starting log fetch for block range {hex_to_int(start_block):,} -> {hex_to_int(end_block):,}")
+    
     logs = fetch_logs(
-        from_block=int_to_hex(24608077),
-        to_block=int_to_hex(24608077),
+        url=INFURA_URL,
+        from_block=start_block,
+        to_block=end_block,
         address=COIN_ADDRESSES,
         topics=[TRANSFER_EVENT_TOPIC],
         logger=logger
     )
     
-    with open(OUTPUT_FOLDER_PATH / "infura_logs.json", 'w') as f:
-        json.dump([decode_log(log) for log in logs], f, indent=4)
+    now_utc = datetime.now(timezone.utc)
+    ts = now_utc.strftime("%Y%m%d_%H%M%S") # gives UTC not Istanbul, "20260308_205135"
+    output_file_path = OUTPUT_FOLDER_PATH / f"infura_logs_{ts}.json"
+
+    with open(output_file_path, 'w') as f:
+        logger.info(f"Decoding {len(logs)} logs")
+        decoded_logs = [decode_log(TOKEN_MAPPING, log) for log in logs]
+        json.dump(decoded_logs, f, indent=4)
+    
+    logger.info(f"Saved {len(decoded_logs)} decoded logs to {output_file_path}")
+    
+    token_counts = Counter([log["token_symbol"] for log in decoded_logs])
+    logger.info(f"Token transfer summary: {dict(token_counts)}")
+    
+    logger.info("Run completed successfully")
 
 
 if __name__ == "__main__":
--- src/helpers.py
+++ src/helpers.py
@@ -13,6 +13,11 @@ def get_token_address(network: Networks, token: StableCoins) -> str:
         data = yaml.safe_load(f)
     return data[token.value]["contracts"][network.value]
 
+def get_decimals(network: Networks, token: StableCoins) -> int:
+    with open(CONFIG_FOLDER_PATH / "tokens.yaml", "r") as f:
+        data = yaml.safe_load(f)
+    return data[token.value]["decimals"]
+
 def get_chain_id(network: Networks):
     with open(CONFIG_FOLDER_PATH / "networks.yaml", "r") as f:
         data = yaml.safe_load(f)
@@ -43,18 +48,40 @@ def is_erc20_transfer(log, should_debug):
     
     return False
      
+
+
 def get_logger(name, level=logging.INFO):
+    class ColoredFormatter(logging.Formatter):
+        COLORS = {
+            "DEBUG": "\033[36m",     # cyan
+            "INFO": "\033[32m",      # green
+            "WARNING": "\033[33m",   # yellow
+            "ERROR": "\033[31m",     # red
+            "CRITICAL": "\033[41m",  # red background
+        }
+        RESET = "\033[0m"
+
+        def format(self, record):
+            color = self.COLORS.get(record.levelname, self.RESET)
+            record.levelname = f"{color}{record.levelname}{self.RESET}"
+            record.msg = f"{color}{record.msg}{self.RESET}"
+            return super().format(record)
+
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    
+
     if not logger.hasHandlers():
         handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        formatter = ColoredFormatter(
+            "%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+            "%Y-%m-%d %H:%M:%S"
+        )
+
         handler.setFormatter(formatter)
         logger.addHandler(handler)
+
     return logger
-
-
 
 ### INFURA HELPERS START ###
 def get_infura_url():
